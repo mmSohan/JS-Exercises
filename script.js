fb65c800292a022202746c9ea7c71f166c789e0f
MD. Masbahul <mesbahul.sohan@gmail.com>
2019-09-30 09:31:42 +0600

    classWork4/task2

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* JavaScript code here */
+console.log("External js");
+
+//Task2
+
+function arraySum(arr){
+    let sum=0;
+    arr.forEach(element => {
+
+        sum +=element;
+
+        
+    });
+
+    return (multiplier) =>{
+        return sum*multiplier;
+    }
+
+}
+let sum = arraySum([1,2,3]);
+console.log(sum(2));
